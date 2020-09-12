c6958af77965b27585121a8f92ece279b88decb2
Rendi K <rendi.krinaldi@gmail.com>
2020-09-12 23:04:36 +0700

    callback chaining & address from console input

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-geocode('Jakarta', (err, data) => {
-  console.log('Error', err)
-  console.log('Data', data)
-})
+// get the address from the console input
+const address = process.argv[2]
 
-forecast(44.1545, -75.7088, (err, data) => {
-  console.log('Error', err)
-  console.log('Data', data)
+if (!address) {
+  return console.log('Please provide an address')
+}
+
+// set the address from console input as the input parameter
+geocode(address, (err, data) => {
+  if (err) {
+    return console.log(err)
+  }
+
+  forecast(data.latitude, data.longitude, (err, forecastData) => {
+    if (err) {
+      return console.log(err)
+    }
+
+    console.log(data.location)
+    console.log(forecastData)
+  })
 })
